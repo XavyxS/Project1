562df6fc6147b0371afe27b7130c2f3bb7c9cc37
Xavy San <francisco.sandoval23b@udgvirtual.udg.mx>
2024-06-29 00:10:52 -0600

    innitial 7

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
-const readline = require('readline');
 const app = express();
 const port = 3000;
-
-// Crear una interfaz readline si estamos en desarrollo
-const isDevelopment = process.env.NODE_ENV !== 'production';
-let rl;
-if (isDevelopment) {
-  rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-}
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -23,40 +12,14 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err.stack);
-    return;
-  }
-  console.log('Connected to MySQL as id ' + connection.threadId);
+  if (err) throw err;
+  console.log('Connected to MySQL!');
+});
 
-  // Verificar la conexión con una consulta simple
-  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-    // Pausar hasta que se presione una tecla, solo en desarrollo
-    if (isDevelopment) {
-      rl.question('Press any key to continue...', (answer) => {
-        rl.close();
-
-        // Continuar con la ejecución del servidor
-        app.get('/', (req, res) => {
-          res.send('Hello World!');
-        });
-
-        app.listen(port, () => {
-          console.log(`Example app listening at http://localhost:${port}`);
-        });
-      });
-    } else {
-      // Continuar con la ejecución del servidor en producción
-      app.get('/', (req, res) => {
-        res.send('Hello World!');
-      });
-
-      app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-      });
-    }
-  });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
