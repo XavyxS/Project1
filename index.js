a5be0e61b33a38052e2dc54fea4b022d625a27ee
Xavy San <francisco.sandoval23b@udgvirtual.udg.mx>
2024-06-28 22:56:12 -0600

    innitial 3

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
+const express = require('express');
 const mysql = require('mysql');
+const app = express();
+const port = 3000;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Compac1984',
-  database: 'db_proj1'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
